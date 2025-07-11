6676ed1e3524f331884f87a753b0372298b6627a
Neill Tuck <neill@orkasolar.co.za>
2025-07-11 16:19:47 +0200

    dummy placeholder

---
+++ backend/migrations/versions/85c5f148d4d9_dummy_placeholder.py
@@ -0,0 +1,20 @@
+"""dummy placeholder for missing revision
+
+Revision ID: 85c5f148d4d9
+Revises: 
+Create Date: 2025-07-11 00:00:00.000000
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = '85c5f148d4d9'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    # nothing: this just satisfies Alembic
+    pass
+
+def downgrade():
+    pass
