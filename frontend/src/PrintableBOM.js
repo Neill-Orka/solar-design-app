09f837262957a06ab71e2671dd41fb701a70da3a
Neill Tuck <neill@orkasolar.co.za>
2025-08-13 09:21:28 +0200

    Before prepaginated BOM Quote

--- frontend/src/PrintableBOM.js
+++ frontend/src/PrintableBOM.js
@@ -27,7 +27,7 @@ function PrintableBOM() {
       {/* Controls (hidden on print) */}
       <div
         className="no-print"
-        style={{ padding: '20px', position: 'fixed', top: '20px', right: '20px' }}
+        style={{ padding: '20px', position: 'fixed', top: '6%', right: '6%' }}
       >
         <Button variant="light" onClick={() => navigate(-1)}>
           Back
@@ -37,148 +37,121 @@ function PrintableBOM() {
         </Button>
       </div>
 
-      {/* Single table: header in <thead> so it repeats on each page */}
-      <table style={{ width: '100%', borderCollapse: 'collapse', fontSize: '11px' }}>
+      {/* Document header (outside the main table) */}
+      <div className="print-header" style={{ marginBottom: '20px' }}>
+        {/* Company / Address */}
+        <div style={{ marginBottom: '10px' }}>
+          <img
+            src={logo}
+            alt="Orka Solar Logo"
+            style={{ height: '40px', width: 'auto', marginBottom: '5px' }}
+          />
+          <div style={{ fontSize: '10px', color: '#555' }}>
+            <p style={{ margin: 0 }}>Orka Solar (Pty) Ltd</p>
+            <p style={{ margin: 0 }}>Reg No: 2017/141572/07</p>
+            <p style={{ margin: 0 }}>VAT No: 443 028 1337</p>
+            <p style={{ margin: 0 }}>
+              T: 082 660 0951&nbsp; E: info@orkasolar.co.za&nbsp; W: www.orkasolar.co.za
+            </p>
+          </div>
+        </div>
+
+        {/* Title */}
+        <div style={{ textAlign: 'center', padding: 0, paddingBottom: '8px' }}>
+          <h1 style={{ fontSize: '20px', margin: 0 }}>Quotation</h1>
+        </div>
+
+        {/* Client / Quote info */}
+        <div style={{ paddingBottom: '8px' }}>
+          <table style={{ width: '100%', borderCollapse: 'separate', borderSpacing: '0 1px' }}>
+            <tbody>
+              <tr>
+                {/* Left block: client */}
+                <td style={{ width: '50%', verticalAlign: 'top' }}>
+                  <table
+                    style={{
+                      width: '100%',
+                      fontSize: '10px',
+                      borderCollapse: 'separate',
+                      borderSpacing: '0 1px'
+                    }}
+                  >
+                    <tbody>
+                      <tr>
+                        <td style={{ width: '120px', fontWeight: 'bold' }}>For attention:</td>
+                        <td>{bomData.project?.client_name || 'Client Name'}</td>
+                      </tr>
+                      <tr>
+                        <td style={{ fontWeight: 'bold' }}>Company:</td>
+                        <td>{bomData.project?.name || 'Company Name'}</td>
+                      </tr>
+                      <tr>
+                        <td style={{ fontWeight: 'bold' }}>Address:</td>
+                        <td>{bomData.project?.location || 'Address'}</td>
+                      </tr>
+                      <tr>
+                        <td style={{ fontWeight: 'bold' }}>Mail:</td>
+                        <td>-</td>
+                      </tr>
+                      <tr>
+                        <td style={{ fontWeight: 'bold' }}>Tel:</td>
+                        <td>{bomData.project?.site_phone || '-'}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+
+                {/* Right block: quote meta */}
+                <td style={{ width: '50%', verticalAlign: 'top' }}>
+                  <table
+                    style={{
+                      width: '100%',
+                      fontSize: '10px',
+                      borderCollapse: 'separate',
+                      borderSpacing: '0 1px'
+                    }}
+                  >
+                    <tbody>
+                      <tr>
+                        <td style={{ width: '120px', fontWeight: 'bold' }}>Date:</td>
+                        <td>{currentDate}</td>
+                      </tr>
+                      <tr>
+                        <td style={{ fontWeight: 'bold' }}>Quote number:</td>
+                        <td>QTE{(bomData.project?.id || '').toString().padStart(8, '0')}</td>
+                      </tr>
+                      <tr>
+                        <td style={{ fontWeight: 'bold' }}>Contact Person:</td>
+                        <td>{bomData.project?.site_contact_person || '-'}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        {/* Project title bar */}
+        <div
+          style={{
+            borderTop: '1px solid #000',
+            borderBottom: '1px solid #000',
+            padding: '5px 0',
+            fontSize: '12px',
+            fontWeight: 'bold'
+          }}
+        >
+          {bomData.project?.name} - {bomData.systemSpecs?.panelKw}kW{' '}
+          {bomData.systemSpecs?.batteryKwh > 0
+            ? `${bomData.systemSpecs?.batteryKwh}kWh Storage`
+            : 'System'}
+        </div>
+      </div>
+
+      {/* BOM table with simple column headers */}
+      <table style={{ width: '100%', borderCollapse: 'collapse', fontSize: '11px', marginTop: '13cm' }}>
         <thead>
-          {/* Company / Address */}
-          <tr>
-            <td colSpan={4} style={{ paddingBottom: '8px' }}>
-              <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-                <tbody>
-                  <tr>
-                    <td style={{ width: '50%', verticalAlign: 'top' }}>
-                      <img
-                        src={logo}
-                        alt="Orka Solar Logo"
-                        style={{ height: '40px', width: 'auto', marginBottom: '5px' }}
-                      />
-                      <div style={{ fontSize: '10px', color: '#555' }}>
-                        <p style={{ margin: 0 }}>Orka Solar (Pty) Ltd</p>
-                        <p style={{ margin: 0 }}>Reg No: 2017/141572/07</p>
-                        <p style={{ margin: 0 }}>VAT No: 443 028 1337</p>
-                        <p style={{ margin: 0 }}>
-                          T: 082 660 0951&nbsp; E: info@orkasolar.co.za&nbsp; W: www.orkasolar.co.za
-                        </p>
-                      </div>
-                    </td>
-                    <td
-                      style={{
-                        width: '50%',
-                        textAlign: 'right',
-                        fontSize: '10px',
-                        color: '#555',
-                        verticalAlign: 'top'
-                      }}
-                    >
-                      <p style={{ margin: 0 }}>3 Retief Street</p>
-                      <p style={{ margin: 0 }}>The Square, Unit 13</p>
-                      <p style={{ margin: 0 }}>Potchefstroom</p>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-
-          {/* Title */}
-          <tr>
-            <td colSpan={4} style={{ textAlign: 'center', padding: 0, paddingBottom: '8px' }}>
-              <h1 style={{ fontSize: '20px', margin: 0 }}>Quotation</h1>
-            </td>
-          </tr>
-
-          {/* Client / Quote info */}
-          <tr>
-            <td colSpan={4} style={{ paddingBottom: '8px' }}>
-              <table style={{ width: '100%', borderCollapse: 'separate', borderSpacing: '0 1px' }}>
-                <tbody>
-                  <tr>
-                    {/* Left block: client */}
-                    <td style={{ width: '50%', verticalAlign: 'top' }}>
-                      <table
-                        style={{
-                          width: '100%',
-                          fontSize: '10px',
-                          borderCollapse: 'separate',
-                          borderSpacing: '0 1px'
-                        }}
-                      >
-                        <tbody>
-                          <tr>
-                            <td style={{ width: '120px', fontWeight: 'bold' }}>For attention:</td>
-                            <td>{bomData.project?.client_name || 'Client Name'}</td>
-                          </tr>
-                          <tr>
-                            <td style={{ fontWeight: 'bold' }}>Company:</td>
-                            <td>{bomData.project?.name || 'Company Name'}</td>
-                          </tr>
-                          <tr>
-                            <td style={{ fontWeight: 'bold' }}>Address:</td>
-                            <td>{bomData.project?.location || 'Address'}</td>
-                          </tr>
-                          <tr>
-                            <td style={{ fontWeight: 'bold' }}>Mail:</td>
-                            <td>-</td>
-                          </tr>
-                          <tr>
-                            <td style={{ fontWeight: 'bold' }}>Tel:</td>
-                            <td>{bomData.project?.site_phone || '-'}</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-
-                    {/* Right block: quote meta */}
-                    <td style={{ width: '50%', verticalAlign: 'top' }}>
-                      <table
-                        style={{
-                          width: '100%',
-                          fontSize: '10px',
-                          borderCollapse: 'separate',
-                          borderSpacing: '0 1px'
-                        }}
-                      >
-                        <tbody>
-                          <tr>
-                            <td style={{ width: '120px', fontWeight: 'bold' }}>Date:</td>
-                            <td>{currentDate}</td>
-                          </tr>
-                          <tr>
-                            <td style={{ fontWeight: 'bold' }}>Quote number:</td>
-                            <td>QTE{(bomData.project?.id || '').toString().padStart(8, '0')}</td>
-                          </tr>
-                          <tr>
-                            <td style={{ fontWeight: 'bold' }}>Contact Person:</td>
-                            <td>{bomData.project?.site_contact_person || '-'}</td>
-                          </tr>
-                        </tbody>
-                      </table>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-
-          {/* Project title bar */}
-          <tr>
-            <td
-              colSpan={4}
-              style={{
-                borderTop: '1px solid #000',
-                borderBottom: '1px solid #000',
-                padding: '5px 0',
-                fontSize: '12px',
-                fontWeight: 'bold'
-              }}
-            >
-              {bomData.project?.name} - {bomData.systemSpecs?.panelKw}kW{' '}
-              {bomData.systemSpecs?.batteryKwh > 0
-                ? `${bomData.systemSpecs?.batteryKwh}kWh Storage`
-                : 'System'}
-            </td>
-          </tr>
-
           {/* Column headers */}
           <tr style={{ backgroundColor: '#f2f2f2' }}>
             <th style={{ textAlign: 'left', padding: '8px', borderBottom: '1px solid #ddd' }}>
@@ -426,34 +399,103 @@ function PrintableBOM() {
 <style type="text/css" media="print">
   {`
   @media print {
-    /* Hard reset: some global CSS is doing body * { visibility:hidden } */
-    html, body, body * {
+    /* Base reset */
+    html, body {
+      margin: 0 !important; 
+      padding: 0 !important;
+    }
+    
+    body * {
       visibility: visible !important;
     }
-
-    /* Hide only the non-print UI */
+    
+    /* Page setup */
+    @page {
+      size: A4;
+      margin: 0cm 1.5cm 1.5cm 1.5cm;
+    }
+    
+    /* Table structure settings */
+    table {
+      margin: 0 !important;
+      padding: 0 !important;
+      page-break-inside: auto;
+      border-collapse: collapse;
+    }
+    
+    /* Critical: Force header repetition */
+    thead {
+      display: table-header-group !important;
+      break-inside: avoid;
+    }
+    
+    /* Make sure headers repeat on each page */
+    thead tr {
+      page-break-inside: avoid;
+    }
+    
+    /* Table body settings */
+    tbody {
+      display: table-row-group;
+    }
+    
+    /* Row break control */
+    tr {
+      page-break-inside: avoid;
+      page-break-after: auto;
+    }
+    
+    /* Space control */
+    thead > tr:first-child {
+      margin-top: 0 !important;
+      padding-top: 0 !important;
+    }
+    
+    thead > tr:first-child > td {
+      margin-top: 0 !important;
+      padding-top: 0 !important;
+    }
+    
+    /* Logo positioning */
+    thead img {
+      display: block;
+      margin-top: 0 !important;
+      padding-top: 0 !important;
+    }
+    
+    /* Hide print controls */
     .no-print {
       display: none !important;
       visibility: hidden !important;
     }
-
-    @page {
-      size: A4;
-      margin: 1.5cm;
-    }
-
-    /* Make the table header repeat on every page */
-    thead { display: table-header-group; }
-
-    /* Normal table pagination */
-    table { page-break-inside: auto; width: 100%; border-collapse: collapse; }
-    tr    { page-break-inside: avoid; page-break-after: auto; }
-
-    /* Ensure greys/backgrounds print */
+    
+    /* Background printing */
     tr[style*="background-color"],
     th, td, div {
       -webkit-print-color-adjust: exact !important;
       print-color-adjust: exact !important;
+    }
+    
+    /* Add page numbers */
+    @page {
+      @bottom-right {
+        content: counter(page);
+      }
+    }
+
+    /* Print header */
+    .print-header {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      z-index: 100;
+      background-color: white;
+    }
+
+    /* Adjust table margin to accommodate fixed header */
+    table {
+      margin-top: 14cm; /* Adjust based on your header height */
     }
   }
   `}
